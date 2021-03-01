332a7c17056805f1c62a71ae6617ef543b6d5743
Joshua Lee <g00373792@gmit.ie>
2021-03-01 17:56:32 +0000

    benchmarks: Add benchmarks
    This adds benchmarks for performance tracking and comparison. Currently, it runs a
    basic comparison of mulib's singly-linked list against those of the popular GLib C
    library.

---
+++ benchmark/benchmark-gslist.c
@@ -0,0 +1,34 @@
+/* benchmark-gslist.c
+ *
+ * Copyright 2021 Joshua Lee <lee.son.wai@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <glib.h>
+#include <stdio.h>
+
+int
+main (int    argc,
+      char **argv)
+{
+  GSList *list;
+
+  for (int i = 0; i < 65536; i++)
+    list = g_slist_append (list, GINT_TO_POINTER (i));
+
+  return 0;
+}
